63fb9a7e3de3b0beed28d2bc7faafd602ae5a194
Sean Zheng <blackhorseya@gmail.com>
2024-06-10 14:19:31 +0800

    chore: add new dependencies and update existing ones in go modules
    - Add new dependencies to `go_deps` in MODULE.bazel
    - Change the dependency path for `com_github_google_uuid` in app/infra/configx/BUILD.bazel
    - Add a new file `app/infra/transports/httpx/BUILD.bazel` with relevant content
    - Add a new file `app/infra/transports/httpx/server.go` with relevant content
    - Update dependencies in `go.mod` for `github.com/gin-contrib/zap`, `github.com/google/uuid`, `go.uber.org/zap`, `go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin`, `github.com/go-logr/logr`, `github.com/go-logr/stdr`, `go.opentelemetry.io/otel`, `go.opentelemetry.io/otel/metric`, `go.opentelemetry.io/otel/trace`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ app/infra/transports/httpx/server.go
@@ -0,0 +1,80 @@
+package httpx
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/blackhorseya/godine/pkg/responsex"
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.uber.org/zap"
+)
+
+// Server is an HTTP server.
+type Server struct {
+	httpserver *http.Server
+	Router     *gin.Engine
+}
+
+// NewServer is used to create a new HTTP server.
+func NewServer() (*Server, error) {
+	ctx := contextx.Background()
+
+	gin.SetMode(configx.A.HTTP.Mode)
+
+	router := gin.New()
+	router.Use(ginzap.GinzapWithConfig(ctx.Logger, &ginzap.Config{
+		TimeFormat: time.RFC3339,
+		UTC:        true,
+		SkipPaths:  []string{"/api/healthz"},
+		Context:    nil,
+	}))
+	router.Use(otelgin.Middleware(configx.A.Name))
+	router.Use(contextx.AddContextxMiddleware())
+	router.Use(responsex.AddErrorHandlingMiddleware())
+	router.Use(ginzap.CustomRecoveryWithZap(ctx.Logger, true, func(c *gin.Context, err any) {
+		responsex.Err(c, fmt.Errorf("%v", err))
+		c.Abort()
+	}))
+
+	httpserver := &http.Server{
+		Addr:              configx.A.HTTP.GetAddr(),
+		Handler:           router,
+		ReadHeaderTimeout: time.Second,
+	}
+
+	return &Server{
+		httpserver: httpserver,
+		Router:     router,
+	}, nil
+}
+
+// Start is used to start the server.
+func (s *Server) Start(ctx contextx.Contextx) error {
+	go func() {
+		err := s.httpserver.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			ctx.Fatal("start http server error", zap.Error(err))
+		}
+	}()
+
+	return nil
+}
+
+// Stop is used to stop the server.
+func (s *Server) Stop(ctx contextx.Contextx) error {
+	timeout, cancelFunc := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancelFunc()
+
+	err := s.httpserver.Shutdown(timeout)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/blackhorseya/godine
 go 1.22.3
 
 require (
+	github.com/gin-contrib/zap v1.1.3
 	github.com/gin-gonic/gin v1.10.0
+	github.com/google/uuid v1.4.0
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.19.0
-	go.uber.org/zap v1.21.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.52.0
+	go.uber.org/zap v1.27.0
 )
 
 require (
@@ -17,6 +20,8 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.21.0 // indirect
@@ -41,7 +46,9 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
