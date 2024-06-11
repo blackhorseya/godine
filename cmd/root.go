10c4bf0bb770b414ea0b334c57d6214f7bbbd650
Sean Zheng <blackhorseya@gmail.com>
2024-06-11 08:34:53 +0800

    chore: refactor configuration handling in cmd package
    - Add `app/infra/configx` as a dependency in `cmd/BUILD.bazel`
    - Remove `github.com/spf13/viper` from dependencies in `cmd/BUILD.bazel`
    - Remove `fmt` and `github.com/spf13/viper` imports in `cmd/root.go`
    - Update the default path for the config file in `cmd/root.go`
    - Replace `viper.SetConfigFile(cfgFile)` with `configx.LoadConfig(cfgFile)` in `cmd/root.go`
    - Remove code related to reading config files from `cmd/root.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,10 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
+	"github.com/blackhorseya/godine/app/infra/configx"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -41,7 +40,12 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.godine.yaml)")
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile,
+		"config",
+		"",
+		"config file (default is $HOME/.config/godine/.godine.yaml)",
+	)
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -50,24 +54,6 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".godine" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".godine")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	err := configx.LoadConfig(cfgFile)
+	cobra.CheckErr(err)
 }
