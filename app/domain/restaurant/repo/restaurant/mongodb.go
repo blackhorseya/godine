a28873af0a91dec79b74fe1817aa004ef62fb02b
Sean Zheng <blackhorseya@gmail.com>
2024-06-11 17:59:04 +0800

    feat: improve error handling and add timeouts to methods
    - Add span and timeout functionality to the Delete method
    - Implement error handling for the Delete method
    - Add span and timeout functionality to the GetByID method
    - Implement error handling for the GetByID method
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/restaurant/repo/restaurant/mongodb.go
+++ app/domain/restaurant/repo/restaurant/mongodb.go
@@ -62,13 +62,37 @@ func (i *mongodb) Update(ctx contextx.Contextx, data *model.Restaurant) (err err
 }
 
 func (i *mongodb) Delete(ctx contextx.Contextx, id string) (err error) {
-	// todo: 2024/6/11|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "restaurant.mongodb.delete")
+	defer span.End()
+
+	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
+	defer cancelFunc()
+
+	filter := bson.M{"_id": id}
+	_, err = i.rw.Database(dbName).Collection(collName).DeleteOne(timeout, filter)
+	if err != nil {
+		ctx.Error("delete restaurant from mongodb failed", zap.Error(err), zap.String("id", id))
+		return err
+	}
+
+	return nil
 }
 
 func (i *mongodb) GetByID(ctx contextx.Contextx, id string) (item *model.Restaurant, err error) {
-	// todo: 2024/6/11|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "restaurant.mongodb.get_by_id")
+	defer span.End()
+
+	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
+	defer cancelFunc()
+
+	filter := bson.M{"_id": id}
+	err = i.rw.Database(dbName).Collection(collName).FindOne(timeout, filter).Decode(&item)
+	if err != nil {
+		ctx.Error("get restaurant by id from mongodb failed", zap.Error(err), zap.String("id", id))
+		return nil, err
+	}
+
+	return item, nil
 }
 
 func (i *mongodb) List(
