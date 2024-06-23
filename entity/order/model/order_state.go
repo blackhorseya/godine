3318ca731eb75c236ef9301f3c55d6ad13706732
Sean Zheng <blackhorseya@gmail.com>
2024-06-23 19:56:37 +0800

    refactor: refactor order state handling and add pending state functionality
    - Remove the Update method from the OrderState interface
    - Add a PendingState struct with a Next method
    - Implement a todo comment in the PendingState Next method
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/order/model/order_state.go
+++ entity/order/model/order_state.go
@@ -6,9 +6,18 @@ import (
 
 // OrderState is the interface for order state.
 type OrderState interface {
-	// Update is used to update the order.
-	Update(ctx contextx.Contextx, order *Order) (event *OrderEvent, err error)
-
 	// Next is the next state of the order.
 	Next(ctx contextx.Contextx, order *Order) (event *OrderEvent, err error)
+}
+
+// PendingState is the pending state of the order.
+type PendingState struct{}
+
+func (s *PendingState) Next(ctx contextx.Contextx, order *Order) (event *OrderEvent, err error) {
+	// todo: 2024/6/23|sean|implement me
+
+	return &OrderEvent{
+		Name:    "pending",
+		Handler: "example",
+	}, nil
 }
