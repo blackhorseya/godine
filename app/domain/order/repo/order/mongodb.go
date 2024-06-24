1efd80f5311e5f4ae0fa298901b2c6ffbbab6c76
Sean Zheng <blackhorseya@gmail.com>
2024-06-24 11:15:44 +0800

    fix: improve error handling and query options in user functions
    - Add error handling and HTTP status code for mongo.ErrNoDocuments in GetByID function
    - Add context and options for query in ListByUserID function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/order/repo/order/mongodb.go
+++ app/domain/order/repo/order/mongodb.go
@@ -1,15 +1,19 @@
 package order
 
 import (
+	"errors"
+	"net/http"
 	"time"
 
 	"github.com/blackhorseya/godine/app/infra/otelx"
 	"github.com/blackhorseya/godine/entity/order/model"
 	"github.com/blackhorseya/godine/entity/order/repo"
 	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/blackhorseya/godine/pkg/errorx"
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -56,6 +60,10 @@ func (i *mongodb) GetByID(ctx contextx.Contextx, id string) (item *model.Order, 
 	filter := bson.M{"_id": id}
 	err = i.rw.Database(dbName).Collection(collName).FindOne(timeout, filter).Decode(item)
 	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, errorx.Wrap(http.StatusNotFound, 404, err)
+		}
+
 		return nil, err
 	}
 
@@ -67,8 +75,39 @@ func (i *mongodb) ListByUserID(
 	userID string,
 	condition repo.ListCondition,
 ) (items []*model.Order, total int, err error) {
-	// todo: 2024/6/13|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "order.mongodb.list_by_user_id")
+	defer span.End()
+
+	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
+	defer cancelFunc()
+
+	filter := bson.M{"user_id": userID}
+
+	opts := options.Find()
+	if condition.Limit > 0 {
+		opts.SetLimit(int64(condition.Limit))
+	}
+	if condition.Offset > 0 {
+		opts.SetSkip(int64(condition.Offset))
+	}
+
+	cursor, err := i.rw.Database(dbName).Collection(collName).Find(timeout, filter, opts)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer cursor.Close(timeout)
+
+	err = cursor.All(timeout, &items)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	count, err := i.rw.Database(dbName).Collection(collName).CountDocuments(timeout, filter)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return items, int(count), nil
 }
 
 func (i *mongodb) ListByRestaurantID(
