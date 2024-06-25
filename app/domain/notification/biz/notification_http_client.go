100632fad92495085abe2963aa0bd640c3660902
Sean Zheng <blackhorseya@gmail.com>
2024-06-26 06:07:55 +0800

    chore: improve dependency management and HTTP client initialization
    - Add `app/infra/configx` and `io_opentelemetry_go_contrib_instrumentation_net_http_otelhttp` to dependencies in `BUILD.bazel`
    - Add `app/infra/configx` import to `notification_http_client.go`
    - Initialize `url` and `client` in `NewNotificationHTTPClient` in `notification_http_client.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/notification/biz/notification_http_client.go
+++ app/domain/notification/biz/notification_http_client.go
@@ -1,17 +1,26 @@
 package biz
 
 import (
+	"net/http"
+
+	"github.com/blackhorseya/godine/app/infra/configx"
 	"github.com/blackhorseya/godine/entity/notification/biz"
 	"github.com/blackhorseya/godine/entity/notification/model"
 	"github.com/blackhorseya/godine/pkg/contextx"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 type notificationHTTPClient struct {
+	url    string
+	client *http.Client
 }
 
 // NewNotificationHTTPClient creates a new notification service.
 func NewNotificationHTTPClient() biz.INotificationBiz {
-	return &notificationHTTPClient{}
+	return &notificationHTTPClient{
+		url:    configx.C.NotifyRestful.HTTP.URL,
+		client: &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)},
+	}
 }
 
 func (i *notificationHTTPClient) CreateNotification(ctx contextx.Contextx, notification *model.Notification) error {
