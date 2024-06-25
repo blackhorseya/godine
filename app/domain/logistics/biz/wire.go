4f6666996489bc0321979ef5b646c30d5a457b8f
Sean Zheng <blackhorseya@gmail.com>
2024-06-25 12:07:10 +0800

    feat: integrate dependency injection with Wire framework
    - Add `wire.go` to the `srcs` list in `biz` go_library in BUILD.bazel
    - Add `@com_github_google_wire//:wire` to the `deps` list in `biz` go_library in BUILD.bazel
    - Add `wire.go` file to the `biz` package with `ProviderLogisticsSet` definition
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ app/domain/logistics/biz/wire.go
@@ -0,0 +1,9 @@
+package biz
+
+import (
+	"github.com/blackhorseya/godine/app/domain/logistics/repo/delivery"
+	"github.com/google/wire"
+)
+
+// ProviderLogisticsSet is biz provider set.
+var ProviderLogisticsSet = wire.NewSet(NewLogistics, delivery.NewMongodb)
