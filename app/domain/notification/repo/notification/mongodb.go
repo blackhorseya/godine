b7ad2d93c972e51771ccbd9802c790367be28b43
Sean Zheng <blackhorseya@gmail.com>
2024-06-26 05:40:59 +0800

    feat: refactor dependency management and notifications in MongoDB integration
    - Add `@org_uber_go_zap//:zap` to the dependencies in `BUILD.bazel`
    - Add `@com_github_google_uuid//:uuid` to the dependencies in `BUILD.bazel`
    - Add `go.uber.org/zap` to the import statements in `mongodb.go`
    - Add `github.com/google/uuid` to the import statements in `mongodb.go`
    - Implement functionality to create and insert a notification in `Create` method in `mongodb.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/notification/repo/notification/mongodb.go
+++ app/domain/notification/repo/notification/mongodb.go
@@ -3,10 +3,13 @@ package notification
 import (
 	"time"
 
+	"github.com/blackhorseya/godine/app/infra/otelx"
 	"github.com/blackhorseya/godine/entity/notification/model"
 	"github.com/blackhorseya/godine/entity/notification/repo"
 	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
 )
 
 const (
@@ -25,8 +28,29 @@ func NewMongodb(rw *mongo.Client) repo.INotificationRepo {
 }
 
 func (i *mongodb) Create(ctx contextx.Contextx, notify *model.Notification) error {
-	// todo: 2024/6/26|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "biz.notification.repo.notification.mongodb.Create")
+	defer span.End()
+
+	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
+	defer cancelFunc()
+
+	if notify.ID == "" {
+		notify.ID = uuid.New().String()
+	}
+	notify.CreatedAt = time.Now()
+	notify.UpdatedAt = time.Now()
+
+	_, err := i.rw.Database(dbName).Collection(collName).InsertOne(timeout, notify)
+	if err != nil {
+		ctx.Error(
+			"insert one notification to mongodb failed",
+			zap.Error(err),
+			zap.Any("notification", &notify),
+		)
+		return err
+	}
+
+	return nil
 }
 
 func (i *mongodb) GetByID(ctx contextx.Contextx, id string) (item *model.Notification, err error) {
