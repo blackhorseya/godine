1d720dee5c06664ab29cf50fc6b7cbd0249111a7
Sean Zheng <blackhorseya@gmail.com>
2024-06-25 13:30:47 +0800

    feat: consolidate dependencies and implement new method
    - Add the `pkg/responsex` dependency to `BUILD.bazel` file
    - Import `bytes`, `encoding/json`, and `errors` in `logistics_http_client.go`
    - Move the `otelx` initialization to `logistics_http_client.go`
    - Implement the `CreateDelivery` method in `logistics_http_client.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/logistics/biz/logistics_http_client.go
+++ app/domain/logistics/biz/logistics_http_client.go
@@ -1,12 +1,18 @@
 package biz
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
+	"net/url"
 
 	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/app/infra/otelx"
 	"github.com/blackhorseya/godine/entity/logistics/biz"
 	"github.com/blackhorseya/godine/entity/logistics/model"
 	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/blackhorseya/godine/pkg/responsex"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
@@ -24,8 +30,47 @@ func NewLogisticsHTTPClient() biz.ILogisticsBiz {
 }
 
 func (i *logisticsHTTPClient) CreateDelivery(ctx contextx.Contextx, delivery *model.Delivery) error {
-	// todo: 2024/6/25|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "biz.logistics.http_client.CreateDelivery")
+	defer span.End()
+
+	ep, err := url.ParseRequestURI(i.url + "/api/v1/deliveries")
+	if err != nil {
+		return err
+	}
+
+	payload, err := json.Marshal(delivery)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, ep.String(), bytes.NewReader(payload))
+	if err != nil {
+		return err
+	}
+
+	resp, err := i.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	type response struct {
+		responsex.Response `json:",inline"`
+		Data               *model.Delivery `json:"data"`
+	}
+	var got response
+	err = json.NewDecoder(resp.Body).Decode(&got)
+	if err != nil {
+		return err
+	}
+
+	if got.Code != http.StatusOK {
+		return errors.New(got.Message)
+	}
+
+	delivery = got.Data
+
+	return nil
 }
 
 func (i *logisticsHTTPClient) UpdateDeliveryStatus(ctx contextx.Contextx, deliveryID string, status string) error {
