4b568982627c0dd528a1ab78d3dbe185cf2f9ace
Sean Zheng <blackhorseya@gmail.com>
2024-06-25 12:28:22 +0800

    feat: add dependencies and implement Create method in mongodb.go
    - Add dependencies `@com_github_google_uuid//:uuid` and `@org_uber_go_zap//:zap` to `BUILD.bazel`
    - Add `time` package import to `mongodb.go`
    - Add constants `defaultTimeout`, `dbName`, and `collName` to `mongodb.go`
    - Implement `Create` method in `mongodb.go` with error handling
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/logistics/repo/delivery/mongodb.go
+++ app/domain/logistics/repo/delivery/mongodb.go
@@ -1,10 +1,21 @@
 package delivery
 
 import (
+	"time"
+
+	"github.com/blackhorseya/godine/app/infra/otelx"
 	"github.com/blackhorseya/godine/entity/logistics/model"
 	"github.com/blackhorseya/godine/entity/logistics/repo"
 	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
+)
+
+const (
+	defaultTimeout = 5 * time.Second
+	dbName         = "godine"
+	collName       = "deliveries"
 )
 
 type mongodb struct {
@@ -17,8 +28,25 @@ func NewMongodb(rw *mongo.Client) repo.IDeliveryRepo {
 }
 
 func (i *mongodb) Create(ctx contextx.Contextx, item *model.Delivery) error {
-	// todo: 2024/6/25|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "biz.logistics.repo.delivery.mongodb.Create")
+	defer span.End()
+
+	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
+	defer cancelFunc()
+
+	if item.ID == "" {
+		item.ID = uuid.New().String()
+	}
+	item.CreatedAt = time.Now()
+	item.UpdatedAt = time.Now()
+
+	_, err := i.rw.Database(dbName).Collection(collName).InsertOne(timeout, item)
+	if err != nil {
+		ctx.Error("insert one delivery to mongodb failed", zap.Error(err), zap.Any("delivery", &item))
+		return err
+	}
+
+	return nil
 }
 
 func (i *mongodb) GetByID(ctx contextx.Contextx, id string) (item *model.Delivery, err error) {
