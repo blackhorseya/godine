406d5471e941503135f02bf8591653f520cb3e60
Sean Zheng <blackhorseya@gmail.com>
2024-06-26 06:12:24 +0800

    feat: refactor notification HTTP client functionality in Go codebase
    - Add `pkg/responsex` to the dependencies in `app/domain/notification/biz/BUILD.bazel`
    - Import `encoding/json` and `errors` in `app/domain/notification/biz/notification_http_client.go`
    - Import `net/url` in `app/domain/notification/biz/notification_http_client.go`
    - Import `github.com/blackhorseya/godine/app/infra/otelx` in `app/domain/notification/biz/notification_http_client.go`
    - Add error handling and HTTP request logic in the `GetNotification` function in `app/domain/notification/biz/notification_http_client.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/notification/biz/notification_http_client.go
+++ app/domain/notification/biz/notification_http_client.go
@@ -1,12 +1,17 @@
 package biz
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
+	"net/url"
 
 	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/app/infra/otelx"
 	"github.com/blackhorseya/godine/entity/notification/biz"
 	"github.com/blackhorseya/godine/entity/notification/model"
 	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/blackhorseya/godine/pkg/responsex"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
@@ -41,8 +46,40 @@ func (i *notificationHTTPClient) GetNotification(
 	ctx contextx.Contextx,
 	notificationID string,
 ) (item *model.Notification, err error) {
-	// todo: 2024/6/26|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "biz.notification.http_client.GetNotification")
+	defer span.End()
+
+	ep, err := url.ParseRequestURI(i.url + "/api/v1/notifications/" + notificationID)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ep.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := i.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	type response struct {
+		responsex.Response `json:",inline"`
+		Data               *model.Notification `json:"data"`
+	}
+	var got response
+	err = json.NewDecoder(resp.Body).Decode(&got)
+	if err != nil {
+		return nil, err
+	}
+
+	if got.Code != http.StatusOK {
+		return nil, errors.New(got.Message)
+	}
+
+	return got.Data, nil
 }
 
 func (i *notificationHTTPClient) ListNotificationsByUser(
