b774275efc04f120391fde832f9bff8d8ac04af3
Sean Zheng <blackhorseya@gmail.com>
2024-06-26 17:00:40 +0800

    feat: implement in-memory event bus with registration and publishing capabilities
    - Add `event_bus_memory.go` to the `srcs` in `mqx` library
    - Change the return type of `Register` and `Unregister` in `event_bus.go`
    - Add `event_bus_memory.go` file with `MemoryEventBus` type
    - Implement `NewMemoryEventBus`, `Register`, `Unregister`, and `Publish` methods in `event_bus_memory.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/mqx/event_bus.go
+++ app/infra/transports/mqx/event_bus.go
@@ -4,9 +4,12 @@ import (
 	"github.com/blackhorseya/godine/entity/events"
 )
 
+// HandlerID defines the type for handler ID.
+type HandlerID int
+
 // EventBus defines the interface for an event bus.
 type EventBus interface {
-	Register(eventType string, handler func(events.DomainEvent))
-	Unregister(eventType string, handler func(events.DomainEvent))
+	Register(eventType string, handler func(events.DomainEvent)) HandlerID
+	Unregister(eventType string, id HandlerID)
 	Publish(event events.DomainEvent)
 }
