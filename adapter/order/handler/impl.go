ac67383c8c745fb615bdfc1d78ced863ae5dc4c1
Sean Zheng <blackhorseya@gmail.com>
2024-06-27 09:10:37 +0800

    feat: implement dependency injection using Wire in handler package
    - Add a new Bazel build file for the `handler` package
    - Implement the `impl` struct and related methods in `impl.go`
    - Add an `Injector` struct and related fields in `injector.go`
    - Implement test cases in `main_test.go`
    - Implement wire injection in `wire.go` and `wire_gen.go`
    - Update the application configuration in `config.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/order/handler/impl.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	_ "github.com/blackhorseya/godine/api/order/restful" // swagger docs
+	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/app/infra/transports/httpx"
+	"github.com/blackhorseya/godine/pkg/adapterx"
+	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/blackhorseya/godine/pkg/responsex"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type impl struct {
+	injector *Injector
+	server   *httpx.Server
+}
+
+func newRestful(injector *Injector, server *httpx.Server) adapterx.Restful {
+	return &impl{injector: injector, server: server}
+}
+
+func (i *impl) Start() error {
+	ctx := contextx.Background()
+
+	err := i.InitRouting()
+	if err != nil {
+		return err
+	}
+
+	err = i.server.Start(ctx)
+	if err != nil {
+		return err
+	}
+
+	ctx.Info("start restful server", zap.String("swagger_url", fmt.Sprintf(
+		"http://%s/api/docs/index.html",
+		strings.ReplaceAll(configx.A.HTTP.GetAddr(), "0.0.0.0", "localhost"),
+	)))
+
+	return nil
+}
+
+func (i *impl) AwaitSignal() error {
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		ctx := contextx.Background()
+		ctx.Info("receive signal", zap.String("signal", sig.String()))
+
+		err := i.server.Stop(ctx)
+		if err != nil {
+			ctx.Error("shutdown restful server error", zap.Error(err))
+		}
+	}
+
+	return nil
+}
+
+func (i *impl) InitRouting() error {
+	router := i.server.Router
+
+	// api
+	api := router.Group("/api")
+	{
+		api.GET("/healthz", i.Healthz)
+	}
+
+	return nil
+}
+
+func (i *impl) GetRouter() *gin.Engine {
+	return i.server.Router
+}
+
+// Healthz is used to check the health of the service.
+// @Summary Check the health of the service.
+// @Description Check the health of the service.
+// @Tags health
+// @Accept json
+// @Produce json
+// @Success 200 {object} responsex.Response
+// @Failure 500 {object} responsex.Response
+// @Router /healthz [get]
+func (i *impl) Healthz(c *gin.Context) {
+	responsex.OK(c, nil)
+}
---
+++ adapter/order/handler/wire.go
@@ -0,0 +1,47 @@
+//go:build wireinject
+
+//go:generate wire
+
+package handler
+
+import (
+	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/app/infra/otelx"
+	"github.com/blackhorseya/godine/app/infra/transports/httpx"
+	"github.com/blackhorseya/godine/pkg/adapterx"
+	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/blackhorseya/godine/pkg/logging"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+func initApplication() (*configx.Application, error) {
+	app, err := configx.LoadApplication(&configx.C.OrderHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	err = logging.Init(app.Log)
+	if err != nil {
+		return nil, err
+	}
+
+	err = otelx.SetupOTelSDK(contextx.Background(), app)
+	if err != nil {
+		return nil, err
+	}
+
+	return app, nil
+}
+
+var providerSet = wire.NewSet(
+	newRestful,
+
+	wire.Struct(new(Injector), "*"),
+	initApplication,
+	httpx.NewServer,
+)
+
+func New(v *viper.Viper) (adapterx.Restful, error) {
+	panic(wire.Build(providerSet))
+}
