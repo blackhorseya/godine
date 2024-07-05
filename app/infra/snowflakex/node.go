d233a7f91687826d91f222f092a55f36321dd0a9
Sean Zheng <blackhorseya@gmail.com>
2024-07-06 07:12:53 +0800

    feat: refactor snowflake node ID generation
    - Modify the way the snowflake node ID is generated
    - Add a random number generator to create the snowflake node ID
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/snowflakex/node.go
+++ app/infra/snowflakex/node.go
@@ -1,14 +1,28 @@
 package snowflakex
 
 import (
+	"crypto/rand"
 	"fmt"
+	"math/big"
 
 	"github.com/bwmarrin/snowflake"
 )
 
 // NewNode create and return a new snowflake node
 func NewNode() (*snowflake.Node, error) {
-	node, err := snowflake.NewNode(1)
+	// Define the range (2^10)
+	maxInt := big.NewInt(1 << 10)
+
+	// Generate a random number in the range [0, 2^10)
+	num, err := rand.Int(rand.Reader, maxInt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert to an integer
+	id := num.Int64()
+
+	node, err := snowflake.NewNode(id)
 	if err != nil {
 		return nil, fmt.Errorf("new snowflake node failed: %w", err)
 	}
