9042d589e45dbd3ab119eb84baf8876b16a3a23a
Sean Zheng <blackhorseya@gmail.com>
2024-07-05 11:20:22 +0800

    feat: refactor test setup and configuration handling
    - Add imports for `configx`, `mariadbx`, `repo`, and `logging`
    - Load configuration and application settings in `SetupTest`
    - Initialize logging and create a new mariadb client in `SetupTest`
    - Set `repo` to a new `Mariadb` instance in `SetupTest`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/order/repo/order/mariadb_external_test.go
+++ app/domain/order/repo/order/mariadb_external_test.go
@@ -5,6 +5,10 @@ package order
 import (
 	"testing"
 
+	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/app/infra/storage/mariadbx"
+	"github.com/blackhorseya/godine/entity/domain/order/repo"
+	"github.com/blackhorseya/godine/pkg/logging"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/suite"
 )
@@ -12,10 +16,25 @@ import (
 type mariadbExternalTester struct {
 	suite.Suite
 
-	rw *sqlx.DB
+	rw   *sqlx.DB
+	repo repo.IOrderRepo
 }
 
 func (s *mariadbExternalTester) SetupTest() {
+	err := configx.LoadConfig("")
+	s.Require().NoError(err)
+
+	app, err := configx.LoadApplication(&configx.C.OrderRestful)
+	s.Require().NoError(err)
+
+	err = logging.Init(app.Log)
+	s.Require().NoError(err)
+
+	rw, err := mariadbx.NewClient(app)
+	s.Require().NoError(err)
+	s.rw = rw
+
+	s.repo = NewMariadb(s.rw)
 }
 
 func (s *mariadbExternalTester) TearDownTest() {
