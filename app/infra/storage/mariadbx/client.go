e97bb306fd56c18d359a22ec68d70d0848d307a1
Sean Zheng <blackhorseya@gmail.com>
2024-07-05 12:49:59 +0800

    chore: update storage dependency to mariadbx across the codebase
    - Update the storage dependency in `BUILD.bazel` from mongodbx to mariadbx
    - Update the storage dependency in `wire.go` from mongodbx to mariadbx
    - Update the storage dependency in `wire_gen.go` from mongodbx to mariadbx
    - Update the storage dependency in `mariadb_external_test.go` from mariadbxV2 to mariadbx
    - Remove the dependency on `jmoiron/sqlx` in `BUILD.bazel` and `client.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/storage/mariadbx/client.go
+++ app/infra/storage/mariadbx/client.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/blackhorseya/godine/app/infra/configx"
 	_ "github.com/go-sql-driver/mysql" // import MySQL driver
-	"github.com/jmoiron/sqlx"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,21 +16,7 @@ const (
 )
 
 // NewClient init mysql client.
-func NewClient(app *configx.Application) (*sqlx.DB, error) {
-	db, err := sqlx.Open("mysql", app.Storage.Mysql.DSN)
-	if err != nil {
-		return nil, fmt.Errorf("open mysql client error: %w", err)
-	}
-
-	db.SetConnMaxLifetime(defaultMaxLifetime)
-	db.SetMaxOpenConns(defaultConns)
-	db.SetMaxIdleConns(defaultConns)
-
-	return db, nil
-}
-
-// NewClientV2 init mysql client.
-func NewClientV2(app *configx.Application) (*gorm.DB, error) {
+func NewClient(app *configx.Application) (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(app.Storage.Mysql.DSN), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("open mysql client error: %w", err)
