13071b2e9a1d13983ce68fb79d2f555af8d577bb
Sean Zheng <blackhorseya@gmail.com>
2024-07-05 11:16:28 +0800

    feat: refactor MariaDB external tester configuration
    - Add a new field `rw` of type `*sqlx.DB` to `mariadbExternalTester`
    - Add `SetupTest` and `TearDownTest` functions to `mariadbExternalTester`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/order/repo/order/mariadb_external_test.go
+++ app/domain/order/repo/order/mariadb_external_test.go
@@ -5,11 +5,23 @@ package order
 import (
 	"testing"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/suite"
 )
 
 type mariadbExternalTester struct {
 	suite.Suite
+
+	rw *sqlx.DB
+}
+
+func (s *mariadbExternalTester) SetupTest() {
+}
+
+func (s *mariadbExternalTester) TearDownTest() {
+	if s.rw != nil {
+		_ = s.rw.Close()
+	}
 }
 
 func TestMariadbExternal(t *testing.T) {
