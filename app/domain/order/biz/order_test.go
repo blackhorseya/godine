82c5dcc6daeaa50b2984d486b80a9bd2301aee77
Sean Zheng <blackhorseya@gmail.com>
2024-07-19 10:16:06 +0800

    test: refactor order functionality and dependencies
    - Add `//entity/domain/order/model` and `//pkg/contextx` to the list of dependencies in `BUILD.bazel`
    - Add error handling and testing for `GetOrder` function in `order_test.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/order/biz/order_test.go
+++ app/domain/order/biz/order_test.go
@@ -1,17 +1,23 @@
 package biz
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
 	logisticsB "github.com/blackhorseya/godine/entity/domain/logistics/biz"
 	notifyB "github.com/blackhorseya/godine/entity/domain/notification/biz"
 	"github.com/blackhorseya/godine/entity/domain/order/biz"
+	model4 "github.com/blackhorseya/godine/entity/domain/order/model"
 	"github.com/blackhorseya/godine/entity/domain/order/repo"
 	rB "github.com/blackhorseya/godine/entity/domain/restaurant/biz"
 	userB "github.com/blackhorseya/godine/entity/domain/user/biz"
+	"github.com/blackhorseya/godine/pkg/contextx"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/mock/gomock"
 )
+
+var errMock = errors.New("mock error")
 
 type suiteTester struct {
 	suite.Suite
@@ -50,4 +56,52 @@ func (s *suiteTester) TearDownTest() {
 
 func TestAll(t *testing.T) {
 	suite.Run(t, new(suiteTester))
+}
+
+func (s *suiteTester) Test_orderBiz_GetOrder() {
+	type args struct {
+		ctx  contextx.Contextx
+		id   string
+		mock func()
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantOrder *model4.Order
+		wantErr   bool
+	}{
+		{
+			name: "get order failed",
+			args: args{id: "1", mock: func() {
+				s.orders.EXPECT().GetByID(gomock.Any(), "1").Return(nil, errMock)
+			}},
+			wantOrder: nil,
+			wantErr:   true,
+		},
+		{
+			name: "get order success",
+			args: args{id: "1", mock: func() {
+				s.orders.EXPECT().GetByID(gomock.Any(), "1").Return(&model4.Order{ID: "1"}, nil)
+			}},
+			wantOrder: &model4.Order{ID: "1"},
+			wantErr:   false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			tt.args.ctx = contextx.Background()
+			if tt.args.mock != nil {
+				tt.args.mock()
+			}
+
+			gotOrder, err := s.biz.GetOrder(tt.args.ctx, tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetOrder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotOrder, tt.wantOrder) {
+				t.Errorf("GetOrder() gotOrder = %v, want %v", gotOrder, tt.wantOrder)
+			}
+		})
+	}
 }
