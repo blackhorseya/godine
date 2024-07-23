7de11d25cf4f6e8122bcb2ba85d4296881fa932c
Sean Zheng <blackhorseya@gmail.com>
2024-07-23 13:17:01 +0800

    chore: refactor authorization logic across files
    - Add `//pkg/contextx` as a dependency in `BUILD.bazel`
    - Add import for `github.com/blackhorseya/godine/pkg/contextx` in `authz.go`
    - Add a check for disabled casbin and log a warning if disabled
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/authz/authz.go
+++ app/infra/authz/authz.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/blackhorseya/godine/app/infra/configx"
+	"github.com/blackhorseya/godine/pkg/contextx"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 )
@@ -15,6 +16,11 @@ type Authz struct {
 
 // New is used to create a new authorization.
 func New(app *configx.Application) (*Authz, error) {
+	if !app.Casbin.Enabled {
+		contextx.Background().Warn("casbin is disabled")
+		return nil, nil //nolint:nilnil // return nil to indicate casbin is disabled
+	}
+
 	var adapter, err = gormadapter.NewAdapter(app.Casbin.PolicyDriver, app.Storage.Mysql.DSN, true)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create casbin adapter: %w", err)
