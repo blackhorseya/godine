c53d9a3317ed75d87fd9e870acbfeadae9bd2afb
Sean Zheng <blackhorseya@gmail.com>
2024-07-25 08:57:09 +0800

    test: test coverage expansion for `biz` package
    - Add a new test file `user_test.go` for the `biz` package
    - Add a new `go_test` target in `BUILD.bazel` for the `biz_test` with dependencies and sources
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ app/domain/user/biz/user_test.go
@@ -0,0 +1,27 @@
+package biz
+
+import (
+	"testing"
+
+	"github.com/blackhorseya/godine/entity/domain/user/biz"
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
+)
+
+type suiteTester struct {
+	suite.Suite
+
+	ctrl *gomock.Controller
+	biz  biz.IUserBiz
+}
+
+func (s *suiteTester) SetupTest() {
+}
+
+func (s *suiteTester) TearDownTest() {
+	s.ctrl.Finish()
+}
+
+func TestAll(t *testing.T) {
+	suite.Run(t, new(suiteTester))
+}
