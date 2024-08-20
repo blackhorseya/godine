63e5585043f46f4c7a7c20d8e21958f13758ac0a
Sean Zheng <blackhorseya@gmail.com>
2024-08-21 00:36:35 +0800

    refactor: refactor server methods and dependencies
    - Update the `GetAddr` method in the `HTTP` struct to use the receiver `x` instead of `http`
    - Add a new `GetAddr` method to the `GRPC` struct
    - Remove dependencies on `authx` and `authz` in the `grpcx` BUILD file
    - Modify the `NewServer` function in `server.go` to no longer require `authx` and `authz` parameters
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/grpcx/server.go
+++ app/infra/transports/grpcx/server.go
@@ -3,8 +3,6 @@ package grpcx
 import (
 	"net"
 
-	"github.com/blackhorseya/godine/app/infra/authx"
-	"github.com/blackhorseya/godine/app/infra/authz"
 	"github.com/blackhorseya/godine/app/infra/configx"
 	"github.com/blackhorseya/godine/pkg/contextx"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
@@ -26,7 +24,7 @@ type Server struct {
 }
 
 // NewServer creates a new grpc server.
-func NewServer(app *configx.Application, init InitServers, authx *authx.Authx, authz *authz.Authz) (*Server, error) {
+func NewServer(app *configx.Application, init InitServers) (*Server, error) {
 	logger := contextx.Background().Logger
 	server := grpc.NewServer(
 		grpc.StatsHandler(otelgrpc.NewServerHandler()),
@@ -34,17 +32,11 @@ func NewServer(app *configx.Application, init InitServers, authx *authx.Authx, a
 			grpc_ctxtags.StreamServerInterceptor(),
 			grpc_zap.StreamServerInterceptor(logger),
 			grpc_recovery.StreamServerInterceptor(),
-			contextx.StreamServerInterceptor(),
-			authx.StreamServerInterceptor(),
-			authz.StreamServerInterceptor(),
 		)),
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			grpc_ctxtags.UnaryServerInterceptor(),
 			grpc_zap.UnaryServerInterceptor(logger),
 			grpc_recovery.UnaryServerInterceptor(),
-			contextx.UnaryServerInterceptor(),
-			authx.UnaryServerInterceptor(),
-			authz.UnaryServerInterceptor(),
 		)),
 	)
 
