64d7a93111f805ed4ac955cf32e9143d52bae3c8
Sean Zheng <blackhorseya@gmail.com>
2024-08-23 14:47:15 +0800

    refactor: refactor client middleware and context handling
    - Update `UnaryClientInterceptor` function in `grpc_client_middleware.go` to use `GetContextx` instead of `FromContext`
    - Add `GetContextx` function to retrieve `Contextx` from `context.Context` in `contextx.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/authx/grpc_client_middleware.go
+++ app/infra/authx/grpc_client_middleware.go
@@ -22,7 +22,7 @@ func (x *Authx) UnaryClientInterceptor() grpc.UnaryClientInterceptor {
 		invoker grpc.UnaryInvoker,
 		opts ...grpc.CallOption,
 	) error {
-		ctx, err := contextx.FromContext(c)
+		ctx, err := contextx.GetContextx(c)
 		if err != nil {
 			return fmt.Errorf("get context error: %w", err)
 		}
@@ -57,7 +57,7 @@ func (x *Authx) StreamClientInterceptor() grpc.StreamClientInterceptor {
 		streamer grpc.Streamer,
 		opts ...grpc.CallOption,
 	) (grpc.ClientStream, error) {
-		ctx, err := contextx.FromContext(c)
+		ctx, err := contextx.GetContextx(c)
 		if err != nil {
 			return nil, fmt.Errorf("get context error: %w", err)
 		}
