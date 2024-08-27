a3a730c93bc74780406961ffa5fafb5c051f211f
Sean Zheng <blackhorseya@gmail.com>
2024-08-27 12:29:42 +0800

    feat: include new dependencies in the project configurations
    - Add a new file `wire_gen.go` to the restaurant biz directory
    - Include `@org_mongodb_go_mongo_driver//mongo` in the test dependencies
    - Include `@com_github_redis_go_redis_v9//:go-redis` in the test dependencies
    - Add `@org_mongodb_go_mongo_driver//mongo` as a dependency
    - Add `@com_github_redis_go_redis_v9//:go-redis` as a dependency
    - Add `@org_golang_google_grpc//credentials/insecure` as a dependency
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/restaurant/biz/restaurant_grpc_server_integration_test.go
+++ app/domain/restaurant/biz/restaurant_grpc_server_integration_test.go
@@ -5,11 +5,89 @@ package biz
 import (
 	"testing"
 
+	"github.com/blackhorseya/godine/app/infra/storage/mongodbx"
+	"github.com/blackhorseya/godine/app/infra/storage/redix"
+	"github.com/blackhorseya/godine/entity/domain/restaurant/biz"
+	"github.com/blackhorseya/godine/pkg/contextx"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
 )
 
 type suiteRestaurantServiceIntegration struct {
 	suite.Suite
+
+	mongodbContainer *mongodbx.Container
+	rw               *mongo.Client
+
+	redisContainer *redix.Container
+	rdb            *redis.Client
+
+	baseServer *grpc.Server
+	server     biz.RestaurantServiceServer
+	client     biz.RestaurantServiceClient
+}
+
+func (s *suiteRestaurantServiceIntegration) SetupTest() {
+	mongodbContainer, err := mongodbx.NewContainer(contextx.Background())
+	s.Require().NoError(err)
+	s.mongodbContainer = mongodbContainer
+
+	rw, err := mongodbContainer.RW(contextx.Background())
+	s.Require().NoError(err)
+	s.rw = rw
+
+	redisContainer, err := redix.NewContainer(contextx.Background())
+	s.Require().NoError(err)
+	s.redisContainer = redisContainer
+
+	rdb, err := redisContainer.RW(contextx.Background())
+	s.Require().NoError(err)
+	s.rdb = rdb
+
+	server, err := NewIntegration(s.rw, s.rdb)
+	s.Require().NoError(err)
+	s.server = server
+
+	buffer := 101024 * 1024
+	listen := bufconn.Listen(buffer)
+	s.baseServer = grpc.NewServer()
+	biz.RegisterRestaurantServiceServer(s.baseServer, s.server)
+	go func() {
+		if err = s.baseServer.Serve(listen); err != nil {
+			s.T().Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	conn, err := grpc.NewClient("", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	s.Require().NoError(err)
+
+	s.client = biz.NewRestaurantServiceClient(conn)
+}
+
+func (s *suiteRestaurantServiceIntegration) TearDownTest() {
+	if s.baseServer != nil {
+		s.baseServer.Stop()
+	}
+
+	if s.rw != nil {
+		_ = s.rw.Disconnect(contextx.Background())
+	}
+
+	if s.rdb != nil {
+		_ = s.rdb.Close()
+	}
+
+	if s.mongodbContainer != nil {
+		_ = s.mongodbContainer.Terminate(contextx.Background())
+	}
+
+	if s.redisContainer != nil {
+		_ = s.redisContainer.Terminate(contextx.Background())
+	}
 }
 
 func TestIntegrationAll(t *testing.T) {
--- app/domain/restaurant/biz/wire.go
+++ app/domain/restaurant/biz/wire.go
@@ -1,8 +1,15 @@
+//go:build wireinject
+
+//go:generate wire
+
 package biz
 
 import (
 	"github.com/blackhorseya/godine/app/domain/restaurant/repo/restaurant"
+	"github.com/blackhorseya/godine/entity/domain/restaurant/biz"
 	"github.com/google/wire"
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // ProviderRestaurantBizSet is a provider set for restaurant biz.
@@ -11,3 +18,7 @@ var ProviderRestaurantBizSet = wire.NewSet(
 	NewMenuService,
 	restaurant.NewMongodb,
 )
+
+func NewIntegration(rw *mongo.Client, rdb *redis.Client) (biz.RestaurantServiceServer, error) {
+	panic(wire.Build(ProviderRestaurantBizSet))
+}
