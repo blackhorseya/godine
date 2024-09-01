e377618fafb1b27e6abdfd43b3d90c8d4fdd7fa7
Sean Zheng <blackhorseya@gmail.com>
2024-09-01 11:23:56 +0800

    refactor: refactor legacy interfaces and update comments
    - Remove legacy interfaces `ServiceLegacy` and `RestfulLegacy`
    - Update comments and remove redundant code in `adapterx.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/adapterx/adapterx.go
+++ pkg/adapterx/adapterx.go
@@ -6,26 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// ServiceLegacy is the interface that wraps the basic Serve method.
-type ServiceLegacy interface {
-	// Start a service asynchronously.
-	Start() error
-
-	// AwaitSignal waits for a signal to shut down the service.
-	AwaitSignal() error
-}
-
-// RestfulLegacy is the interface that wraps the restful api method.
-type RestfulLegacy interface {
-	ServiceLegacy
-
-	// InitRouting init the routing of restful api.
-	InitRouting() error
-
-	// GetRouter returns the router of restful api.
-	GetRouter() *gin.Engine
-}
-
 // Server is the interface that wraps the Serve method.
 type Server interface {
 	// Start the server.
